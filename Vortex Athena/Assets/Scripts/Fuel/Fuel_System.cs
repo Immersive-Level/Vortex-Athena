94f08aa6d622b5a12153a81e3819679c6004d7a4
Santyimmersive <santiago.henao@immersivefoley.com>
2025-05-13 19:33:50 -0500

    Cambio Script Fuel System

--- Vortex Athena/Assets/Scripts/Fuel/Fuel_System.cs
+++ Vortex Athena/Assets/Scripts/Fuel/Fuel_System.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System; // <- para Action
 
 
 public class Fuel_System : MonoBehaviour
@@ -11,6 +12,11 @@ public class Fuel_System : MonoBehaviour
 
     private float currentFuel;
 
+    public bool HasFuel => currentFuel > 0f;  // ¿Queda gasolina?
+
+    // Evento que lanzamos al agotarse
+    public event Action OnFuelEmpty;
+
     void Start()
     {
         currentFuel = maxFuel; // Inicia con el tanque lleno
@@ -19,11 +25,20 @@ public class Fuel_System : MonoBehaviour
 
     public void ConsumeFuel()
     {
-        if (currentFuel > 0)
+        
+        
+        if (currentFuel <= 0f) return;
         {
             currentFuel -= fuelConsumptionRate * Time.deltaTime;
-            currentFuel = Mathf.Clamp(currentFuel, 0, maxFuel);
+            currentFuel = Mathf.Clamp(currentFuel, 0f, maxFuel);
             UpdateFuelBar();
+
+
+            if (currentFuel <= 0f)
+            {
+                OnFuelEmpty?.Invoke();
+            }
+
         }
     }
 

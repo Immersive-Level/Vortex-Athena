88f29c57813ef233acd8537af0f700e31a63f736
Nicolas Forero <nicolasforero998@gmail.com>
2025-08-06 21:56:28 -0500

    Tuto Done
    Tutorial Simple - Se pueden descartar cambios a escena, ya que quedo como prefab.

--- Vortex Athena/Assets/Scripts/Systems/GameManager.cs
+++ Vortex Athena/Assets/Scripts/Systems/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour
     //sistemas hijos del GameManager
     public ScoreSystem ScoreSystem;
 
+    //Objetos tutorial
+    public GameObject _tutorialRoot;     //tutorialRoot debe estar desactivado al inicio
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -17,11 +20,29 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
         }
+
+        Debug.Log("Toggling tutorial");
+        ToggleTutorial();
     }
 
     public void Start()
     {
         
+    }
+
+    //Logica que inica flujo de Tutorial
+    public void ToggleTutorial()
+    {
+        if (Time.timeScale != 0f)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+        Debug.Log("Current time scale is: " + Time.timeScale);
+
+        if (!_tutorialRoot.activeInHierarchy)
+            _tutorialRoot.SetActive(true);
+        else
+            _tutorialRoot.SetActive(false);
     }
 
 }

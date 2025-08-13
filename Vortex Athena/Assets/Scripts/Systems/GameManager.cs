500080d0add864cd0c64ecfb236acd8bbacd8738
Nicolas Forero <nicolasforero998@gmail.com>
2025-08-12 21:17:43 -0500

    Tuto Ajustado (New)
    Hay resto de cambios al meta de varios sprites (no he notado que se explota despues de testeo)
    Escena: añadir TutorialRoot a canvas
    GameManager: Lineas de codigo de control de tutorial flow

--- Vortex Athena/Assets/Scripts/Systems/GameManager.cs
+++ Vortex Athena/Assets/Scripts/Systems/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
     public ScoreSystem ScoreSystem;
     public List<GameObject> NavesActivas { get; private set; }
 
+    public GameObject TutorialRoot;
+
 
     private void Awake()
     {
@@ -25,6 +27,8 @@ public class GameManager : MonoBehaviour
     public void Start()
     {
         NavesActivas = new List<GameObject>();
+
+        ToggleTutorial();
     }
 
     public void RegisterShip(GameObject inShip)
@@ -37,4 +41,16 @@ public class GameManager : MonoBehaviour
         NavesActivas.Remove(inShip);
     }
 
+    public void ToggleTutorial()
+    {
+        if (Time.timeScale != 0)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+
+        if (!TutorialRoot.activeInHierarchy)
+            TutorialRoot.SetActive(true);
+        else
+            TutorialRoot.SetActive(false);
+    }
 }
